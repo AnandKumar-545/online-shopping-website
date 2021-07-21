b3c409c29afd71acb85a2b6be48ecd4276dbd3cb
G. ANAND KUMAR <83350222+AnandKumar-545@users.noreply.github.com>
2021-07-21 14:14:58 +0530

    Update database.js

--- online-shop/util/database.js
+++ online-shop/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('online-shop', 'root', 'Anand@789', {
+const sequelize = new Sequelize('online-shop', 'root', '#password', {
   dialect: 'mysql',
   host: 'localhost'
 });
